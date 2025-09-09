271f52eba818cec7bff89085f799c59d9d1ba760
Matheus <141074019+M00NH0RS3@users.noreply.github.com>
2025-09-09 01:38:52 +0000

    test 2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("pg-cache-v1").then(cache => {
+    caches.open("linst-cache-v1").then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
